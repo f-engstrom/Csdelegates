259ea28b6cf7b476fa97502a62adea6ce87e247c
Frans Engström <frans.engstrom@learnet.se>
2021-01-19 12:16:54 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace Csdelegates
+{
+    class Program
+    {
+
+        delegate int PeformCalculation(int x, int y);
+       
+
+       public static Func<int, int, int> addition = (i, i1) => i+i1 ;
+        static void Main(string[] args)
+        {
+
+
+
+            PeformCalculation operation = new PeformCalculation(addition);
+
+
+
+
+        }
+
+        static int Addition(int x, int y)
+        {
+
+            return x + y;
+
+        }
+
+        static int Multiplication(int x, int y)
+        {
+
+            return x * y;
+
+        }
+    }
+}
